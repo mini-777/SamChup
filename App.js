9a72ccd4a057a2e316ba67590800f551f01054aa
mini-777 <pho0570@gmail.com>
2023-05-25 04:19:51 +0900

    레이아웃 수정

--- App.js
+++ App.js
@@ -1,65 +1,109 @@
-import { NativeBaseProvider, HStack, Center, StatusBar, Pressable, Box, Text } from 'native-base';
-import { StyleSheet, View } from 'react-native';
+import { NativeBaseProvider, HStack, Center, Pressable, Box, Heading, Text, Image, Input, Icon } from 'native-base';
+import { StyleSheet } from 'react-native';
+import { Ionicons } from "@expo/vector-icons";
 
-const buttonComponent = (isPressed, isHovered, buttonIndex) => {
-  return (
-    <Box h="148" w="148" bg={isPressed ? "muted.200" : isHovered ? "muted.200" : "muted.50"} style={{
-      transform: [{
-        scale: isPressed ? 0.96 : 1
-      }]
-    }} p="5" rounded="8" shadow={9} borderWidth="1" borderColor="coolGray.300"><Text>재고 관리</Text>
-    </Box>
-  );
-}
+
+
+
 
 export default function App() {
   return (
     <>
-      <StatusBar bg="#3700B3" barStyle="light-content" />
-      <View style={styles.container}>
-        <NativeBaseProvider>
-          <HStack margin={4} space={8} justifyContent="center">
-            <Center>
-              <Pressable>
-                {({
-                  isHovered,
-                  isPressed
-                }) => {
-                  return buttonComponent(isHovered, isPressed)
-                }}
-              </Pressable>
-            </Center>
-            <Center>
-              <Pressable>
-                {({
-                  isHovered,
-                  isPressed
-                }) => {
-                  return buttonComponent(isHovered, isPressed)
-                }}
-              </Pressable>
-            </Center>
-          </HStack>
-          <HStack margin={4} space={8} justifyContent="center">
-            <Center><Pressable>
+      <NativeBaseProvider >
+        <Box safeArea>
+          <Box margin={4}>
+            <Heading >
+              BARP
+            </Heading>
+
+          </Box>
+          <Box alignSelf="center">
+            <Input h={10} placeholder="재고 검색" variant="filled" width="90%" borderRadius="10" py="1" px="2" InputLeftElement={<Icon ml="2" size="4" color="gray.400" as={<Ionicons name="ios-search" />} />} />
+          </Box>
+          <Box alignItems="center">
+
+
+
+            <Center margin={4}><Pressable>
               {({
                 isHovered,
                 isPressed
               }) => {
-                return buttonComponent(isHovered, isPressed)
+                return <Center h="280" w="330" bg={isPressed ? "muted.200" : isHovered ? "muted.200" : "muted.50"} style={{
+                  transform: [{
+                    scale: isPressed ? 0.96 : 1
+                  }]
+                }} p="5" rounded="20" shadow={9} borderWidth="1" borderColor="coolGray.300"><Text fontWeight='medium' textAlign='center' fontSize='3xl'  >바코드 출고</Text>
+                  <Image source={{
+                    uri: 'https://cdn-icons-png.flaticon.com/512/4396/4396530.png'
+                  }} alt="Barcode Image" size="2xl" h={190} />
+                </Center>
               }}
             </Pressable></Center>
-            <Center><Pressable>
-              {({
-                isHovered,
-                isPressed
-              }) => {
-                return buttonComponent(isHovered, isPressed)
-              }}
-            </Pressable></Center>
-          </HStack>
-        </NativeBaseProvider>
-      </View>
+            <HStack margin={4} space={8} justifyContent="center">
+              <Center >
+                <Pressable >
+                  {({
+                    isHovered,
+                    isPressed
+                  }) => {
+                    return <Center alignItems="center" h="148" w="148" bg={isPressed ? "muted.200" : isHovered ? "muted.200" : "muted.50"} style={{
+                      transform: [{
+                        scale: isPressed ? 0.96 : 1
+                      }]
+                    }} p="5" rounded="20" shadow={9} borderWidth="1" borderColor="coolGray.300" _text={{ fontWeight: 'medium', textAlign: 'center', fontSize: '2xl', }}>재고 관리
+                    </Center>
+                  }}
+                </Pressable>
+              </Center>
+              <Center>
+                <Pressable>
+                  {({
+                    isHovered,
+                    isPressed
+                  }) => {
+                    return <Center h="148" w="148" bg={isPressed ? "muted.200" : isHovered ? "muted.200" : "muted.50"} _text={{ fontWeight: 'medium', textAlign: 'center', fontSize: '2xl', }} style={{
+                      transform: [{
+                        scale: isPressed ? 0.96 : 1
+                      }]
+                    }} p="5" rounded="20" shadow={9} borderWidth="1" borderColor="coolGray.300">재고 내역
+                    </Center>
+                  }}
+                </Pressable>
+              </Center>
+            </HStack>
+            <HStack margin={4} space={8} justifyContent="center">
+              <Center><Pressable>
+                {({
+                  isHovered,
+                  isPressed
+                }) => {
+                  return <Center h="148" w="148" bg={isPressed ? "muted.200" : isHovered ? "muted.200" : "muted.50"} style={{
+                    transform: [{
+                      scale: isPressed ? 0.96 : 1
+                    }]
+                  }} p="5" rounded="20" shadow={9} borderWidth="1" borderColor="coolGray.300" _text={{ fontWeight: 'medium', textAlign: 'center', fontSize: '2xl', }}>발주
+                  </Center>
+                }}
+              </Pressable></Center>
+              <Center><Pressable>
+                {({
+                  isHovered,
+                  isPressed
+                }) => {
+                  return <Center h="148" w="148" bg={isPressed ? "muted.200" : isHovered ? "muted.200" : "muted.50"} style={{
+                    transform: [{
+                      scale: isPressed ? 0.96 : 1
+                    }]
+                  }} p="5" rounded="20" shadow={9} borderWidth="1" borderColor="coolGray.300" _text={{ fontWeight: 'medium', textAlign: 'center', fontSize: '2xl', }}>AI 매니저
+                  </Center>
+                }}
+              </Pressable></Center>
+            </HStack>
+
+          </Box>
+        </Box>
+      </NativeBaseProvider >
     </>
   );
 }
