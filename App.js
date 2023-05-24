b830d1bbe10bbaaf91df0729528bc8113f3c4022
mini-777 <pho0570@gmail.com>
2023-05-24 22:31:54 +0900

    [update] Main grid layout

--- App.js
+++ App.js
@@ -1,12 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider, HStack, Center, StatusBar, Pressable, Box } from 'native-base';
+import { StyleSheet, View } from 'react-native';
+
+const button_component = (isPressed, isHovered) => {
+  return (
+    <Box h="148" w="148" bg={isPressed ? "muted.200" : isHovered ? "muted.200" : "muted.50"} style={{
+      transform: [{
+        scale: isPressed ? 0.96 : 1
+      }]
+    }} p="5" rounded="8" shadow={9} borderWidth="1" borderColor="coolGray.300">
+    </Box>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar bg="#3700B3" barStyle="light-content" />
+      <View style={styles.container}>
+        <NativeBaseProvider>
+          <HStack margin={4} space={8} justifyContent="center">
+            <Center>
+              <Pressable>
+                {({
+                  isHovered,
+                  isPressed
+                }) => {
+                  return button_component(isHovered, isPressed)
+                }}
+              </Pressable>
+            </Center>
+            <Center h="148" w="148" bg="muted.50" rounded="md" shadow={9}></Center>
+          </HStack>
+          <HStack margin={4} space={8} justifyContent="center">
+            <Center h="148" w="148" bg="muted.50" rounded="md" shadow={9} />
+            <Center h="148" w="148" bg="muted.50" rounded="md" shadow={9} />
+          </HStack>
+        </NativeBaseProvider>
+      </View>
+    </>
   );
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # SamChup
 겟잇 삼첩 프로젝트
+
+## Getting Started
+
+### Requirements
+
+`Node JS 18.16.0`
