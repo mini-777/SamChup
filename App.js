bab2f355cad11bcf03d4522c6b66e074f4bb7ca5
mini-777 <pho0570@gmail.com>
2023-05-24 23:00:08 +0900

    commit

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-import { NativeBaseProvider, HStack, Center, StatusBar, Pressable, Box } from 'native-base';
+import { NativeBaseProvider, HStack, Center, StatusBar, Pressable, Box, Text } from 'native-base';
 import { StyleSheet, View } from 'react-native';
 
-const button_component = (isPressed, isHovered) => {
+const buttonComponent = (isPressed, isHovered, buttonIndex) => {
   return (
     <Box h="148" w="148" bg={isPressed ? "muted.200" : isHovered ? "muted.200" : "muted.50"} style={{
       transform: [{
         scale: isPressed ? 0.96 : 1
       }]
-    }} p="5" rounded="8" shadow={9} borderWidth="1" borderColor="coolGray.300">
+    }} p="5" rounded="8" shadow={9} borderWidth="1" borderColor="coolGray.300"><Text>재고 관리</Text>
     </Box>
   );
 }
@@ -25,15 +25,38 @@ export default function App() {
                   isHovered,
                   isPressed
                 }) => {
-                  return button_component(isHovered, isPressed)
+                  return buttonComponent(isHovered, isPressed)
                 }}
               </Pressable>
             </Center>
-            <Center h="148" w="148" bg="muted.50" rounded="md" shadow={9}></Center>
+            <Center>
+              <Pressable>
+                {({
+                  isHovered,
+                  isPressed
+                }) => {
+                  return buttonComponent(isHovered, isPressed)
+                }}
+              </Pressable>
+            </Center>
           </HStack>
           <HStack margin={4} space={8} justifyContent="center">
-            <Center h="148" w="148" bg="muted.50" rounded="md" shadow={9} />
-            <Center h="148" w="148" bg="muted.50" rounded="md" shadow={9} />
+            <Center><Pressable>
+              {({
+                isHovered,
+                isPressed
+              }) => {
+                return buttonComponent(isHovered, isPressed)
+              }}
+            </Pressable></Center>
+            <Center><Pressable>
+              {({
+                isHovered,
+                isPressed
+              }) => {
+                return buttonComponent(isHovered, isPressed)
+              }}
+            </Pressable></Center>
           </HStack>
         </NativeBaseProvider>
       </View>
